25af44fa66e08c80c9e58d2ee17d08e2c4241419
atayoshi <79740951+atayoshi@users.noreply.github.com>
2023-01-27 09:17:01 +0900

    let const

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import "./styles.css";
+/**
+ * const, letの変数宣言
+ */
+// var val1 = 'varを宣言';
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// let val2 = "let変数";
+// console.log(val2);
+
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// const val4 = {
+//   name: "adachi",
+//   age: 28
+// };
+
+// val4.name = "じゃけぇ";
+// val4.addres = "hiroshima";
+// console.log(val4);
+
+// const val5 = ['dog', 'cat'];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
